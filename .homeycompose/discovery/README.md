8a992df94a5aa30248578f9cb0a408132f62ef01
Robin Trietsch <robin.trietsch@gmail.com>
2023-10-06 14:22:52 +0200

    fix: use network scan to find the Quatt CIC

--- .homeycompose/discovery/README.md
+++
@@ -1,7 +0,0 @@
-# Quatt CIC network discovery
-
-As the Quatt CIC doesn't broadcast its presence through mDNS, nor via SSDP, we need to discover it via MAC address ranges.
-The list of MAC addresses is based on the fact that my own Quatt CIC MAC address is identified as Sunplus Technology Co., Ltd.
-
-A list of other MAC address ranges for Sunplus can be found [here](https://udger.com/resources/mac-address-vendor-detail?name=sunplus_technology_co-ltd).
-These ranges have been added to the discovery of the CIC. Please create an issue if there are false positives.
--- drivers/quatt_heatpump/driver.ts
+++ drivers/quatt_heatpump/driver.ts
@@ -1,4 +1,6 @@
 import Homey from 'homey';
+import axios from "axios";
+import {Socket} from "net";
 
 class QuattHeatpumpDriver extends Homey.Driver {
 
@@ -44,28 +46,92 @@ class QuattHeatpumpDriver extends Homey.Driver {
         // }
     }
 
-    /**
-     * TODO should also be able to select a device if the mac address is not in the known prefixes
-     */
     async onPairListDevices() {
-        const discoveryStrategy = this.getDiscoveryStrategy();
-        this.log('discoveryStrategy', discoveryStrategy);
-        const discoveryResults = discoveryStrategy.getDiscoveryResults();
+        let quattIP = await this.findQuattDevice();
+        let quattMAC = await this.homey.arp.getMAC(quattIP)
 
-        this.homey.app.log('discoveryResults', discoveryResults);
+        return [
+            {
+                name: "Quatt CIC",
+                data: {
+                    id: quattMAC,
+                },
+                store: {
+                    address: quattIP,
+                },
+            }
+        ];
+    }
 
-        return Object.values(discoveryResults)
-            .map(discoveryResult => {
-                return {
-                    name: "Quatt Heatpump",
-                    data: {
-                        id: discoveryResult.id,
-                    },
-                    store: {
-                        address: discoveryResult.address,
-                    },
-                };
+    /**
+     * As the Quatt CIC doesn't broadcast its presence through mDNS, nor via SSDP, we need to discover it via MAC address ranges.
+     * However, I've used the MAC address discovery strategy in the past, with all the MAC address prefixes as defined for Sunplus technologies (that's probably the manufacturer of the network card used in the Quatt CIC) (as defined here: https://udger.com/resources/mac-address-vendor-detail?name=sunplus_technology_co-ltd), but one day it stopped detecting the Quatt CIC.
+     *
+     * Therefore, I've setup a simple network scan, which scans the local network for the Quatt CIC, by trying to connect to port 8080 on all IP addresses in the local subnet. If the port is open, we try to fetch data from the candidate device, and if that succeeds, we assume it's the Quatt CIC.
+     */
+    private async findQuattDevice(): Promise<string> {
+        let homeyAddress = await this.homey.cloud.getLocalAddress();
+        let lan = homeyAddress.split('.').slice(0, 3).join('.');
+        this.log('lan', lan)
+
+        let quattCandidates: string[] = [];
+        let quattIP: string | null = null;
+
+        for (let i = 1; i <= 255; i++) {
+            let host = `${lan}.${i}`;
+
+            let socket = new Socket();
+            let status: string | null = null;
+
+            // Socket connection established, port is open
+            socket.on('connect', function () {
+                status = 'open';
+                socket.end();
             });
+            socket.setTimeout(1500);// If no response, assume port is not listening
+            socket.on('timeout', function () {
+                status = 'closed';
+                quattCandidates.splice(quattCandidates.indexOf(host), 1);
+                socket.destroy();
+            });
+            socket.on('error', (exception) => {
+                status = 'closed';
+                quattCandidates.splice(quattCandidates.indexOf(host), 1);
+            });
+            socket.on('close', async (exception) => {
+                if (status == 'open') {
+                    if (await this.verifyIsQuatt(host)) {
+                        quattIP = host;
+                    }
+                }
+
+                quattCandidates.splice(quattCandidates.indexOf(host), 1);
+            });
+
+            quattCandidates.push(host);
+            socket.connect(8080, host);
+        }
+
+        while (quattCandidates.length > 0) {
+            await new Promise(resolve => setTimeout(resolve, 25));
+        }
+
+        if (quattIP) {
+            return quattIP;
+        } else {
+            this.homey.app.log('No Quatt device found on the local network');
+            throw new Error('No Quatt device found on the local network');
+        }
+    }
+
+    private async verifyIsQuatt(address: string) {
+        try {
+            const response = await axios.get(`http://${address}:8080/beta/feed/data.json`);
+            const data = response.data;
+            return true
+        } catch (error) {
+            return false
+        }
     }
 
 }
