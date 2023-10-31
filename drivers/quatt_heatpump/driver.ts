1785b3c0b201e9a568a8813d85d32fc64cca227f
Robin Trietsch <robin.trietsch@gmail.com>
2023-10-31 08:39:33 +0100

    feat: auto reconnect

--- drivers/quatt_heatpump/driver.ts
+++ drivers/quatt_heatpump/driver.ts
@@ -1,6 +1,4 @@
 import Homey from 'homey';
-import axios from "axios";
-import {Socket} from "net";
 import {QuattClient} from "../../lib/quatt";
 
 class QuattHeatpumpDriver extends Homey.Driver {
@@ -9,47 +7,12 @@ class QuattHeatpumpDriver extends Homey.Driver {
      * onInit is called when the driver is initialized.
      */
     async onInit() {
-        // try {
-        //   const settings = this.homey.getSettings();
-        //
-        //   this.homey.app.log('[Device] - init =>', this.homey.getName());
-        //
-        //   this.homey.app.setDebugLogging(settings.debug);
-        //
-        //   if(!settings.mac || settings.mac.length < 8) {
-        //     await this.findMacAddress();
-        //   }
-        //
-        //   if(!settings.encrypted_password || !settings.passwd.includes('+')) {
-        //     await this.savePassword({...settings, encrypted_password: true, encrypted_password_fix: true });
-        //   }
-        //
-        //   await this.checkCapabilities();
-        //
-        //   await this.setSynoClient();
-        //
-        //   this.registerCapabilityListener('onoff', this.onCapability_ON_OFF.bind(this));
-        //   this.registerCapabilityListener('onoff_override', this.onCapability_ON_OFF_OVERRIDE.bind(this));
-        //   this.registerCapabilityListener('action_reboot', this.onCapability_REBOOT.bind(this));
-        //   this.registerCapabilityListener('action_update_data', this.onCapability_UPDATE_DATA.bind(this));
-        //
-        //   await this.checkOnOffState();
-        //   await this.setCapabilityValues();
-        //
-        //   if(settings.enable_interval) {
-        //     await this.checkOnOffStateInterval(settings.update_interval);
-        //     await this.setCapabilityValuesInterval(settings.update_interval);
-        //   }
-        //
-        //   await this.setAvailable();
-        // } catch (error) {
-        //   this.homey.app.log(`[Device] ${this.getName()} - OnInit Error`, error);
-        // }
     }
 
     async onPairListDevices() {
-        let quattIP = await this.findQuattDevice();
-        let quattMAC = await this.homey.arp.getMAC(quattIP)
+        let homeyAddress = await this.homey.cloud.getLocalAddress();
+        let quattIP = await QuattClient.discover(homeyAddress);
+        let quattMAC = await this.homey.arp.getMAC(quattIP);
 
         return [
             {
@@ -63,77 +26,6 @@ class QuattHeatpumpDriver extends Homey.Driver {
             }
         ];
     }
-
-    /**
-     * As the Quatt CIC doesn't broadcast its presence through mDNS, nor via SSDP, we need to discover it via MAC address ranges.
-     * However, I've used the MAC address discovery strategy in the past, with all the MAC address prefixes as defined for Sunplus technologies (that's probably the manufacturer of the network card used in the Quatt CIC) (as defined here: https://udger.com/resources/mac-address-vendor-detail?name=sunplus_technology_co-ltd), but one day it stopped detecting the Quatt CIC.
-     *
-     * Therefore, I've setup a simple network scan, which scans the local network for the Quatt CIC, by trying to connect to port 8080 on all IP addresses in the local subnet. If the port is open, we try to fetch data from the candidate device, and if that succeeds, we assume it's the Quatt CIC.
-     */
-    private async findQuattDevice(): Promise<string> {
-        let homeyAddress = await this.homey.cloud.getLocalAddress();
-        let lan = homeyAddress.split('.').slice(0, 3).join('.');
-        this.log('lan', lan)
-
-        let quattCandidates: string[] = [];
-        let quattIP: string | null = null;
-
-        for (let i = 1; i <= 255; i++) {
-            let host = `${lan}.${i}`;
-
-            let socket = new Socket();
-            let status: string | null = null;
-
-            // Socket connection established, port is open
-            socket.on('connect', function () {
-                status = 'open';
-                socket.end();
-            });
-            socket.setTimeout(1500);// If no response, assume port is not listening
-            socket.on('timeout', function () {
-                status = 'closed';
-                quattCandidates.splice(quattCandidates.indexOf(host), 1);
-                socket.destroy();
-            });
-            socket.on('error', (exception) => {
-                status = 'closed';
-                quattCandidates.splice(quattCandidates.indexOf(host), 1);
-            });
-            socket.on('close', async (exception) => {
-                if (status == 'open') {
-                    if (await this.verifyIsQuatt(host)) {
-                        quattIP = host;
-                    }
-                }
-
-                quattCandidates.splice(quattCandidates.indexOf(host), 1);
-            });
-
-            quattCandidates.push(host);
-            socket.connect(8080, host);
-        }
-
-        while (quattCandidates.length > 0) {
-            await new Promise(resolve => setTimeout(resolve, 25));
-        }
-
-        if (quattIP) {
-            return quattIP;
-        } else {
-            this.homey.app.log('No Quatt device found on the local network');
-            throw new Error('No Quatt device found on the local network');
-        }
-    }
-
-    private async verifyIsQuatt(address: string) {
-        try {
-            let client = new QuattClient(address);
-            return await client.getCicStats(false) != null;
-        } catch (error) {
-            return false
-        }
-    }
-
 }
 
 module.exports = QuattHeatpumpDriver;
--- lib/quatt/index.ts
+++ lib/quatt/index.ts
@@ -1,29 +1,107 @@
 import axios from "axios";
 import {CicStats} from "./cic-stats";
+import {Socket} from "net";
 
 
 export class QuattClient {
     private deviceAddress: string;
+    private readonly homeyAddress: string;
+    private connectionFailures: number = 0;
+    private reconnectionAttempts: number = 0;
 
-    constructor(deviceAddress: string) {
+    constructor(deviceAddress: string, homeyAddress: string) {
         this.deviceAddress = deviceAddress;
+        this.homeyAddress = homeyAddress;
     }
 
-    setDeviceAddress(deviceAddress: string) {
-        this.deviceAddress = deviceAddress;
-    }
-
-    async getCicStats(shouldLog: boolean = true): Promise<CicStats | null> {
+    async getCicStats(shouldLog: boolean = true, shouldReconnect: boolean = true): Promise<CicStats | null> {
         try {
             const response = await axios.get(`http://${this.deviceAddress}:8080/beta/feed/data.json`);
 
             return response.data as CicStats;
         } catch (error) {
+            this.connectionFailures++;
+
             if (shouldLog) {
                 console.log(`Error fetching data from ${this.deviceAddress}`);
                 console.log(error);
             }
+
+            if (this.connectionFailures <= 5 && this.reconnectionAttempts <= 5 && shouldReconnect) {
+                console.log(`Failed to connect to ${this.deviceAddress} 5 times in a row, resetting device address`);
+                this.reconnectionAttempts++;
+                this.deviceAddress = await QuattClient.discover(this.homeyAddress);
+                return this.getCicStats(shouldLog);
+            }
+
             return null;
+        }
+    }
+
+    /**
+     * As the Quatt CIC doesn't broadcast its presence through mDNS, nor via SSDP, we need to discover it via MAC address ranges.
+     * However, I've used the MAC address discovery strategy in the past, with all the MAC address prefixes as defined for Sunplus technologies (that's probably the manufacturer of the network card used in the Quatt CIC) (as defined here: https://udger.com/resources/mac-address-vendor-detail?name=sunplus_technology_co-ltd), but one day it stopped detecting the Quatt CIC.
+     *
+     * Therefore, I've setup a simple network scan, which scans the local network for the Quatt CIC, by trying to connect to port 8080 on all IP addresses in the local subnet. If the port is open, we try to fetch data from the candidate device, and if that succeeds, we assume it's the Quatt CIC.
+     */
+    static async discover(homeyAddress: string): Promise<string> {
+        let lan = homeyAddress.split('.').slice(0, 3).join('.');
+
+        let quattCandidates: string[] = [];
+        let quattIP: string | null = null;
+
+        for (let i = 1; i <= 255; i++) {
+            let host = `${lan}.${i}`;
+
+            let socket = new Socket();
+            let status: string | null = null;
+
+            // Socket connection established, port is open
+            socket.on('connect', function () {
+                status = 'open';
+                socket.end();
+            });
+            socket.setTimeout(1500);// If no response, assume port is not listening
+            socket.on('timeout', function () {
+                status = 'closed';
+                quattCandidates.splice(quattCandidates.indexOf(host), 1);
+                socket.destroy();
+            });
+            socket.on('error', (exception) => {
+                status = 'closed';
+                quattCandidates.splice(quattCandidates.indexOf(host), 1);
+            });
+            socket.on('close', async (exception) => {
+                if (status == 'open') {
+                    if (await this.verifyIsQuatt(host, homeyAddress)) {
+                        quattIP = host;
+                    }
+                }
+
+                quattCandidates.splice(quattCandidates.indexOf(host), 1);
+            });
+
+            quattCandidates.push(host);
+            socket.connect(8080, host);
+        }
+
+        while (quattCandidates.length > 0) {
+            await new Promise(resolve => setTimeout(resolve, 25));
+        }
+
+        if (quattIP) {
+            return quattIP;
+        } else {
+            throw new Error('No Quatt device found on the local network');
+        }
+    }
+
+    private static async verifyIsQuatt(address: string, homeyAddress: string) {
+        try {
+            let client = new QuattClient(address, homeyAddress);
+            return await client.getCicStats(false, false) != null;
+        } catch (error) {
+            return false
         }
     }
 }
