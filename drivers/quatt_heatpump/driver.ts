8c0720677d3e55e0a9705b96e355cf6005da42db
Robin Trietsch <robin.trietsch@gmail.com>
2024-04-21 10:17:52 +0200

    fix(main): remove arp mac lookup, replace with hostname from cic response

--- drivers/quatt_heatpump/driver.ts
+++ drivers/quatt_heatpump/driver.ts
@@ -4,63 +4,29 @@ import {QuattClient} from "../../lib/quatt";
 
 class QuattHeatpumpDriver extends Homey.Driver {
 
-    /**
-     * onInit is called when the driver is initialized.
-     */
     async onInit() {
-        // try {
-        //   const settings = this.homey.getSettings();
-        //
-        //   this.homey.app.log('[Device] - init =>', this.homey.getName());
-        //
-        //   this.homey.app.setDebugLogging(settings.debug);
-        //
-        //   if(!settings.mac || settings.mac.length < 8) {
-        //     await this.findMacAddress();
-        //   }
-        //
-        //   if(!settings.encrypted_password || !settings.passwd.includes('+')) {
-        //     await this.savePassword({...settings, encrypted_password: true, encrypted_password_fix: true });
-        //   }
-        //
-        //   await this.checkCapabilities();
-        //
-        //   await this.setSynoClient();
-        //
-        //   this.registerCapabilityListener('onoff', this.onCapability_ON_OFF.bind(this));
-        //   this.registerCapabilityListener('onoff_override', this.onCapability_ON_OFF_OVERRIDE.bind(this));
-        //   this.registerCapabilityListener('action_reboot', this.onCapability_REBOOT.bind(this));
-        //   this.registerCapabilityListener('action_update_data', this.onCapability_UPDATE_DATA.bind(this));
-        //
-        //   await this.checkOnOffState();
-        //   await this.setCapabilityValues();
-        //
-        //   if(settings.enable_interval) {
-        //     await this.checkOnOffStateInterval(settings.update_interval);
-        //     await this.setCapabilityValuesInterval(settings.update_interval);
-        //   }
-        //
-        //   await this.setAvailable();
-        // } catch (error) {
-        //   this.homey.app.log(`[Device] ${this.getName()} - OnInit Error`, error);
-        // }
     }
 
     async onPairListDevices() {
-        let quattIP = await this.findQuattDevice();
-        let quattMAC = await this.homey.arp.getMAC(quattIP)
+        try {
+            let {ip, hostname} = await this.findQuattDevice();
+            return [
+                {
+                    name: "Quatt CIC",
+                    data: {
+                        id: hostname,
+                    },
+                    store: {
+                        address: ip,
+                    },
+                }
+            ];
+        } catch (e) {
+            // FIXME ensure that the fallback first tries to use a provided static ip for the Quatt CiC
+            this.homey.log("Error while discovering Quatt CiC, falling back to no devices.", e);
 
-        return [
-            {
-                name: "Quatt CIC",
-                data: {
-                    id: quattMAC,
-                },
-                store: {
-                    address: quattIP,
-                },
-            }
-        ];
+            return [];
+        }
     }
 
     /**
@@ -69,13 +35,14 @@ class QuattHeatpumpDriver extends Homey.Driver {
      *
      * Therefore, I've setup a simple network scan, which scans the local network for the Quatt CIC, by trying to connect to port 8080 on all IP addresses in the local subnet. If the port is open, we try to fetch data from the candidate device, and if that succeeds, we assume it's the Quatt CIC.
      */
-    private async findQuattDevice(): Promise<string> {
+    private async findQuattDevice(): Promise<QuattDetails> {
         let homeyAddress = await this.homey.cloud.getLocalAddress();
         let lan = homeyAddress.split('.').slice(0, 3).join('.');
         this.log('lan', lan)
 
         let quattCandidates: string[] = [];
         let quattIP: string | null = null;
+        let quattHostname: string | null = null;
 
         for (let i = 1; i <= 255; i++) {
             let host = `${lan}.${i}`;
@@ -100,8 +67,10 @@ class QuattHeatpumpDriver extends Homey.Driver {
             });
             socket.on('close', async (exception) => {
                 if (status == 'open') {
-                    if (await this.verifyIsQuatt(host)) {
+                    let hostname = await this.getQuattHostname(host);
+                    if (hostname !== undefined) {
                         quattIP = host;
+                        quattHostname = hostname;
                     }
                 }
 
@@ -116,23 +85,29 @@ class QuattHeatpumpDriver extends Homey.Driver {
             await new Promise(resolve => setTimeout(resolve, 25));
         }
 
-        if (quattIP) {
-            return quattIP;
+        if (quattIP && quattHostname) {
+            return {ip: quattIP, hostname: quattHostname};
         } else {
             this.homey.app.log('No Quatt device found on the local network');
             throw new Error('No Quatt device found on the local network');
         }
     }
 
-    private async verifyIsQuatt(address: string) {
+    private async getQuattHostname(address: string) {
         try {
             let client = new QuattClient(this.homey.app.manifest.version, address);
-            return await client.getCicStats(false) != null;
+            let stats = await client.getCicStats(false);
+
+            return stats?.system.hostName
         } catch (error) {
-            return false
+            return undefined;
         }
     }
+}
 
+interface QuattDetails {
+    ip: string;
+    hostname: string;
 }
 
 module.exports = QuattHeatpumpDriver;
